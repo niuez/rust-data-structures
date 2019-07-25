c520dfe8abc2c1beb606bdcd9f1e22ac854b8475
kutimoti <kutimoti.tokura@gmail.com>
2019-07-25 16:00:39 +0900

    [fix] dyn seg tree more faster

--- src/segment_tree/dynamic_segment_tree.rs
+++ src/segment_tree/dynamic_segment_tree.rs
@@ -1,90 +1,141 @@
-use algebra::*;
-
-pub struct Node<M: Monoid> {
-    left: Option<Box<Node<M>>>,
-    right: Option<Box<Node<M>>>,
-    val: M,
+pub trait Magma: Sized + Clone {
+  fn op(&self, rhs: &Self) -> Self;
 }
 
-impl<M: Monoid> Node<M> {
+pub trait Associative: Magma {}
+
+pub trait Unital: Magma {
+  fn identity() -> Self;
+}
+
+pub trait Monoid: Magma + Associative + Unital {}
+
+impl<T: Magma + Associative + Unital> Monoid for T {}
+
+pub enum Node<T: Monoid> {
+    Section(Box<Section<T>>),
+    Leaf(Leaf<T>),
+    None,
+}
+
+pub struct Section<T: Monoid> {
+    left: Node<T>,
+    right: Node<T>,
+    val: T,
+}
+
+pub struct Leaf<T: Monoid> {
+    i: usize,
+    val: T,
+}
+
+impl<T: Monoid> Leaf<T> {
+    fn new(i: usize, x: T) -> Self {
+        Leaf { i: i, val: x }
+    }
+    fn fold(&self) -> T { self.val.clone() }
+}
+
+impl<T: Monoid> Section<T> {
     fn new() -> Self {
-        Node {
-            left: None,
-            right: None,
-            val: M::identity(),
+        Section {
+            left: Node::None,
+            right: Node::None,
+            val: T::identity(),
         }
     }
-}
-
-pub fn value<M: Monoid>(node: &Option<Box<Node<M>>>) -> M {
-    match node.as_ref() {
-        Some(n) => n.as_ref().val.clone(),
-        None => M::identity()
-    }
-}
-
-pub fn update_node<M: Monoid>(node: &mut Node<M>, i: usize, x: M, l: usize, r: usize) {
-    if l + 1 == r {
-        node.val = x;
-    }
-    else {
+    fn fold(&self) -> T { self.val.clone() }
+    fn update(&mut self, i: usize, x: T, l: usize, r: usize) {
         let m = (l + r) >> 1;
         if i < m {
-            {
-                if node.left.is_none() {
-                    node.left = Some(Box::new(Node::new()));
-                }
-                let left = node.left.as_mut().unwrap().as_mut();
-                update_node(left, i, x, l, m);
-            }
+            let left = self.left.take();
+            self.left = left.update(i, x, l, m);
         }
         else {
-            {
-                if node.right.is_none() {
-                    node.right = Some(Box::new(Node::new()));
+            let right = self.right.take();
+            self.right = right.update(i, x, m, r);
+        }
+        self.val = self.left.fold().op(&self.right.fold());
+    }
+}
+
+impl<T: Monoid> Node<T> {
+    fn take(&mut self) -> Node<T> {
+        std::mem::replace(self, Node::None)
+    }
+    fn fold(&self) -> T {
+        match self {
+            &Node::Section(ref sec) => sec.as_ref().fold(),
+            &Node::Leaf(ref leaf) => leaf.fold(),
+            &Node::None => T::identity(),
+        }
+    }
+    fn update(self, i: usize, x: T, l: usize, r: usize) -> Self {
+        match self {
+            Node::Section(mut sec) => {
+                sec.as_mut().update(i, x, l, r);
+                Node::Section(sec)
+            }
+            Node::Leaf(leaf) => {
+                if leaf.i == i {
+                    Node::Leaf(Leaf::new(i, x))
                 }
-                let right = node.right.as_mut().unwrap().as_mut();
-                update_node(right, i, x, m, r);
+                else {
+                    let mut new_section = Section::new();
+                    let m = (l + r) >> 1;
+                    if leaf.i < m {
+                        new_section.left = Node::Leaf(leaf);
+                    }
+                    else {
+                        new_section.right = Node::Leaf(leaf);
+                    }
+                    new_section.update(i, x, l, r);
+                    Node::Section(Box::new(new_section))
+                }
+            }
+            Node::None => {
+                Node::Leaf(Leaf::new(i, x))
             }
         }
-        node.val = value(&node.left).op(&value(&node.right));
+    }
+    fn range_fold(&self, a: usize, b: usize, l: usize, r: usize) -> T {
+        match self {
+            &Node::Section(ref sec) => {
+                if b <= l || r <= a { T::identity() }
+                else if a <= l && r <= b { sec.fold() }
+                else {
+                    let m = (l + r) >> 1;
+                    sec.left.range_fold(a, b, l, m).op(&sec.right.range_fold(a, b, m, r))
+                }
+            }
+            &Node::Leaf(ref leaf) => {
+                if a <= leaf.i && leaf.i < b { leaf.fold() }
+                else { T::identity() }
+            }
+            &Node::None => T::identity(),
+        }
     }
 }
 
-pub fn fold<M: Monoid>(node: &Node<M>, a: usize, b: usize, l: usize, r: usize) -> M {
-    if a <= l && r <= b { node.val.clone() }
-    else if r <= a || b <= l { M::identity() }
-    else {
-        match node.left.as_ref() {
-            Some(le) => fold(le, a, b, l, (l + r) >> 1),
-            None => M::identity(),
-        }.op(& match node.right.as_ref() {
-            Some(ri) => fold(ri, a, b, (l + r) >> 1, r),
-            None => M::identity(),
-        })
-    }
-}
-
-pub struct DynamicSegmentTree<M: Monoid> {
-    root: Node<M>,
+pub struct DynamicSegmentTree<T: Monoid> {
+    root: Node<T>,
     n: usize,
 }
 
-impl<M: Monoid> DynamicSegmentTree<M> {
-    pub fn new(n: usize) -> Self {
-        let mut sz = 1;
-        while sz < n { sz = sz << 1; }
+impl<T: Monoid> DynamicSegmentTree<T> {
+    pub fn new(sz: usize) -> Self {
+        let mut n = 1;
+        while n < sz { n = n << 1; }
         DynamicSegmentTree {
-            root: Node::new(),
-            n: sz
+            root: Node::None,
+            n: n,
         }
     }
-
-    pub fn update(&mut self, i: usize, x: M) {
-        update_node(&mut self.root, i, x, 0, self.n);
+    pub fn update(&mut self, i: usize, x: T) {
+        let r = self.root.take();
+        self.root = r.update(i, x, 0,  self.n);
     }
-
-    pub fn fold(&self, a: usize, b: usize) -> M {
-        fold(&self.root, a, b, 0, self.n)
+    pub fn fold(&self, l: usize, r: usize) -> T {
+        self.root.range_fold(l, r, 0, self.n)
     }
 }
